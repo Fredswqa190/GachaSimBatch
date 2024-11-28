0db583eb094e2808ce3ac7b92635523b6a5de013
AnnabelleYChan <chana8@rpi.edu>
2024-11-28 14:47:16 -0500

    main menu, wish and repeat animation

--- window.py
+++ window.py
@@ -16,12 +16,20 @@ class MyApp(object):
         self.canvas.pack()
 
         self.stop_animation = False
+        self.max_rolls = 0
+        self.count_rolls = 0
         
         self.button_frame = Frame(self.main_frame)
         self.button_frame.pack(side=BOTTOM)
-        self.drawbutton = Button(self.button_frame, text="Roll", \
-                                 command = self.roll)
+        self.drawbutton = Button(self.button_frame, text="1 Pull", \
+                                 command = self.roll_one)
         self.drawbutton.pack(side=LEFT)
+        self.drawbutton = Button(self.button_frame, text="10 Pull", \
+                                 command = self.roll_ten)
+        self.drawbutton.pack(side=LEFT)
+        self.quitbutton = Button(self.button_frame, text="Main Menu", \
+                                 command = self.main_menu)
+        self.quitbutton.pack(side=RIGHT)
         self.quitbutton = Button(self.button_frame, text="Quit", \
                                  command = self.quit)
         self.quitbutton.pack(side=RIGHT)
@@ -32,7 +40,7 @@ class MyApp(object):
     def rand_num(self):
         return 1
 
-    def update_gif(self, frame, img, canvas_img):
+    def update_gif(self, frame, img, canvas_img, stop):
         if self.stop_animation:
             self.canvas.delete("all")
             self.stop_animation = False
@@ -42,26 +50,80 @@ class MyApp(object):
         update = ImageTk.PhotoImage(img)
         self.canvas.itemconfig(canvas_img, image=update)
         self.canvas.image = update
-        self.parent.after(10, self.update_gif, (frame + 1) % img.n_frames, img, canvas_img)
 
-    def roll(self):
-        self.stop_animation = True
-        self.animate()
-        
-    def animate(self):
-        if self.canvas.find_all():
-            self.parent.after(2, self.animate)
+        if (stop and frame == img.n_frames-1):
+            self.canvas.delete("all")
+            self.stop_animation = False
             return
-        
-        image_path = "./"+str(self.rand_num())+".gif"
-        img = Image.open(image_path)
+        self.parent.after(10, self.update_gif, (frame + 1) % img.n_frames, img, canvas_img, stop)
 
+    def animate(self, pathname):
+        img = Image.open(pathname)
         img_tk = ImageTk.PhotoImage(img)
         canvas_img = self.canvas.create_image(0, 0, anchor=NW, image=img_tk)
-        self.canvas.image = img_tk    
+        self.canvas.image = img_tk
+
+        return [canvas_img, img]
+    
+    def main_menu(self):
+        self.stop_animation = True
+        if self.canvas.find_all():
+            self.parent.after(2, self.main_menu)
+            return
+        
+        [canvas_img, img] = self.animate("./mainmenu.gif")
+        self.stop_animation = False
+        self.update_gif(0, img, canvas_img, False)
+        
+    def roll_one(self):
+        self.stop_animation = True
+        self.max_rolls = 1
+        self.intro()
+
+    def roll_ten(self):
+        self.stop_animation = True
+        self.max_rolls = 10
+        self.intro()
+        
+    # buttons shouldn't be visible
+    def intro(self):
+        if self.canvas.find_all():
+            self.parent.after(2, self.intro)
+            return
+        
+        num = self.rand_num() # determines which intro and which character
+        [canvas_img, img] = self.animate("./intro1.gif")
+        self.count_rolls += 1
 
         self.stop_animation = False
-        self.update_gif(0, img, canvas_img)
+        self.update_gif(0, img, canvas_img, True)
+
+        self.character(num)
+
+    def character(self, num):
+        if self.canvas.find_all():
+            self.parent.after(1, self.character, num)
+            return
+
+        # character intro animation
+        [canvas_img, img] = self.animate("./"+str(num)+".gif")
+        # character screen
+        self.stop_animation = False
+        self.update_gif(0, img, canvas_img, True)
+
+        self.repeat(num)
+
+    def repeat(self, num):
+        if self.canvas.find_all():
+            self.parent.after(2, self.repeat, num)
+            return
+
+        # on-click event -> main menu or next character intro
+        # if max_rolls == count_rolls -> main menu
+        [canvas_img, img] = self.animate("./"+str(num)+"repeat.gif")
+        self.stop_animation = False
+        self.update_gif(0, img, canvas_img, False)
+
         
         
 ### The main code simply creates a canvas and three buttons. 
