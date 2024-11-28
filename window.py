02d2d127d0bfbd5461ea46ae2bdf18d38fdea0d6
AnnabelleYChan <chana8@rpi.edu>
2024-11-28 01:07:41 -0500

    Can change to different gifs with the previous one being in a continuous loop

--- window.py
+++ window.py
@@ -14,6 +14,8 @@ class MyApp(object):
         self.canvas = Canvas(self.main_frame, \
                              width=self.width, height=self.height)
         self.canvas.pack()
+
+        self.stop_animation = False
         
         self.button_frame = Frame(self.main_frame)
         self.button_frame.pack(side=BOTTOM)
@@ -27,25 +29,38 @@ class MyApp(object):
     def quit(self):
         self.parent.destroy()
     
+    def rand_num(self):
+        return 1
+
     def update_gif(self, frame, img, canvas_img):
-        if frame != img.n_frames:
-            img.seek(frame)  # Move to the next frame
-            update = ImageTk.PhotoImage(img)
-            self.canvas.itemconfig(canvas_img, image=update)
-            self.canvas.image = update
-            self.parent.after(10, self.update_gif, frame+1, img, canvas_img)
+        if self.stop_animation:
+            self.canvas.delete("all")
+            self.stop_animation = False
+            return
+
+        img.seek(frame)  # Move to the next frame
+        update = ImageTk.PhotoImage(img)
+        self.canvas.itemconfig(canvas_img, image=update)
+        self.canvas.image = update
+        self.parent.after(10, self.update_gif, (frame + 1) % img.n_frames, img, canvas_img)
 
     def roll(self):
-        self.canvas.delete("all")
-
-        num = 1
-        image_path = "./"+str(num)+".gif"
+        self.stop_animation = True
+        self.animate()
+        
+    def animate(self):
+        if self.canvas.find_all():
+            self.parent.after(2, self.animate)
+            return
+        
+        image_path = "./"+str(self.rand_num())+".gif"
         img = Image.open(image_path)
 
         img_tk = ImageTk.PhotoImage(img)
         canvas_img = self.canvas.create_image(0, 0, anchor=NW, image=img_tk)
         self.canvas.image = img_tk    
 
+        self.stop_animation = False
         self.update_gif(0, img, canvas_img)
         
         
